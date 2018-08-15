2f120d700cfe39b641925860db9bbd82df84601b
d_train16 <diepganguly@Gmail.com>
2018-08-15 12:47:48 +1000

    reformatting featureCounts

--- RNA/RNAseq_featureCounts.sh
+++ RNA/RNAseq_featureCounts.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# Use featureCounts to assign counts to annotated features from aligned BAM files
+
+# Use featureCounts to assign counts to annotated features (e.g. genes, transposons) from aligned BAM files
 
 set -eu
 
 if [ "$#" -lt 5 ]; then
 echo "Missing arguments!"
-echo "USAGE: RNAseq_featureCounts.sh <filename> <SE/PE> <0/1/2> <bedfile> <outname>"
-echo "EXAMPLE: RNAseq_featureCounts.sh col0-r1.sorted.bam PE 1 /home/diep/Araport11/annotations/Araport11_mRNA.bed mRNA"
-echo "0 = unstranded, 1 = stranded, 2 = reverse stranded"
+echo "USAGE: RNAseq_featureCounts.sh <filename> <SE/PE> <library strandedness> <bedfile> <bedfile format> <outname>"
+echo "EXAMPLE: RNAseq_featureCounts.sh col0-r1.sorted.bam PE 1 AtRTD2_19April2016.gtf gtf RTD2"
+echo "library strandedness: 0 = unstranded, 1 = stranded, 2 = reverse stranded"
+echo "format: saf, bed, gtf"
 exit 1
 fi
 
@@ -15,44 +17,98 @@ sample=$1
 layout=$2
 strand=$3
 bedfile=$4
-outname=$5
+format=$5
+outname=$6
 
 echo ""
 echo "sample = $1"
 echo "layout = $2"
 echo "strand = $3"
-echo "bedfile = $4 ($5)"
+echo "bedfile = $4 ($5) ($6)"
 echo ""
-echo "$layout $strand featureCounts on $bedfile ($outname) in $sample ..."
+echo "$layout $strand featureCounts on $bedfile $format ($outname) in $sample ..."
 echo ""
 
-# SAF formatted file from BED file anno
-awk -F'\t' '{print $4"\t"$1"\t"$2"\t"$3"\t"$6}' $bedfile > temp.saf
-awk 'BEGIN {print "GeneID""\t""Chr""\t""Start""\t""End""\t""Strand"}{print}' temp.saf > temp2.saf
-
-if [[ $layout == "SE" ]]; then 
-
-featureCounts\
-	-F 'SAF'\
-	-C\
-	-T 2\
-	-s $strand\
-	-a temp2.saf\
-	-o "${1%%.bam*}_${outname}.counts"\
-	$sample 2>&1 | tee -a ../*log
-fi
+if [[ $format == "saf" ]]; then
 	
-if [[ $layout == "PE" ]]; then 
+	if [[ $layout == "SE" ]]; then
+		featureCounts\
+			-F 'SAF'\
+			-C\
+			-T 2\
+			-s $strand\
+			-a temp2.saf\
+		        -o "${1%%.bam*}_${outname}.counts"\
+		        $sample 2>&1 | tee -a ../*log
+	fi
 
-featureCounts\
-	-F SAF\
-	-p\
-	-C\
-	-T 2\
-	-s $strand\
-	-a temp2.saf\
-	-o "${1%%.bam*}_${outname}.counts"\
-	$sample 2>&1 | tee -a ../*log
+	if [[ $layout == "PE" ]]; then
+		featureCounts\
+			-F SAF\
+			-p\
+			-C\
+			-T 2\
+			-s $strand\
+			-a temp2.saf\
+			-o "${1%%.bam*}_${outname}.counts"\
+			$sample 2>&1 | tee -a ../*log
+	fi
+
+fi
+
+if [[ $format == "bed" ]]; then
+	## convert BED to SAF format
+	awk -F'\t' '{print $4"\t"$1"\t"$2"\t"$3"\t"$6}' $bedfile > temp.saf
+	awk 'BEGIN {print "GeneID""\t""Chr""\t""Start""\t""End""\t""Strand"}{print}' temp.saf > temp2.saf
+	
+	if [[ $layout == "SE" ]]; then 
+		featureCounts\
+			-F 'SAF'\
+			-C\
+			-T 2\
+			-s $strand\
+			-a temp2.saf\
+			-o "${1%%.bam*}_${outname}.counts"\
+			$sample 2>&1 | tee -a ../*log
+	fi
+		
+	if [[ $layout == "PE" ]]; then
+	       featureCounts\
+		       -F SAF\
+			-p\
+			-C\
+			-T 2\
+			-s $strand\
+			-a temp2.saf\
+			-o "${1%%.bam*}_${outname}.counts"\
+			$sample 2>&1 | tee -a ../*log
+	fi
+
+fi
+
+if [[ $format == "gtf" ]]; then
+	if [[ $layout == "SE" ]]; then
+		featureCounts\
+			-F GTF\
+			-C\
+			-T 4\
+			-s $strand\
+			-a $bedfile\
+			-o "${1%%.bam*}_${outname}.counts"\
+			$sample 2>&1 | tee -a ../*log
+	fi
+	
+	if [[ $layout == "PE" ]]; then
+		featureCounts\
+			-F GTF\
+			-p\
+			-C\
+			-T 2\
+			-s $strand\
+			-a $bedfile\
+			-o "${1%%.bam*}_${outname}.counts"\
+		        $sample 2>&1 | tee -a ../*log
+	fi
 fi
 
 rm temp*.saf -v
---
+++ RNA/RNAseq_featureCounts_exon.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Perform featureCounts on aligned BAM files to assign transcript counts PER EXON based on the AtRTD2 reference transcript dataset (or quasi - for alternative splicing)
+# Additional info https://www.biostars.org/p/321379/
+# https://www.bioconductor.org/packages/devel/bioc/vignettes/edgeR/inst/doc/edgeRUsersGuide.pdf 
+# Zhang R et al (2017). A high quality Arabidopsis transcriptome for accurate transcript-level analysis of alternative splicing. Nucleic Acids Res. 45: 5061–5073.
+
+set -eu
+
+if [ "$#" -lt 4 ]; then
+echo "Missing arguments!"
+echo "USAGE: RNAseq_featureCounts.sh <filename> <SE/PE> <library strandedness> <rtd2 or padded>"
+echo "EXAMPLE: RNAseq_featureCounts.sh col0-r1.sorted.bam PE 1 "
+echo "library strandedness: 0 = unstranded, 1 = stranded, 2 = reverse stranded"
+exit 1
+fi
+
+sample=$1
+layout=$2
+strand=$3
+ref=$4
+
+if [[ $ref == "rtd2" ]]; then
+	bedfile="$HOME/AtRTD2/AtRTD2_19April2016.gtf"
+	outname="RTD2.counts"
+elif [[ $ref == "padded" ]]; then
+	bedfile="$HOME/AtRTD2/AtRTDv2_QUASI_19April2016.gtf"
+	outname="quasi.counts"
+else
+	echo "bad argument - pick 'rtd2' vs 'padded'"
+	exit 1
+fi
+
+echo ""
+echo "sample = $1"
+echo "layout = $2"
+echo "strand = $3 where 0 = unstranded, 1 = stranded, 2 = reverse stranded"
+echo "$bedfile"
+echo ""
+echo "Exon feature counting - $layout $strand featureCounts on $bedfile in $sample ..."
+echo ""
+
+if [[ $layout == "SE" ]]; then 
+
+featureCounts\
+	-F GTF\
+	-C\		# Do not count read pairs matching different chromosomes
+	-T 4\
+	-f\ 		# Perform read counting at feature level (eg. exons rather than genes).
+	-t exon\	# feature = exon
+	-g gene_id\
+	-O\     	# Reads assigned to multiple meta-features (exons)
+	-s $strand\
+	-a $bedfile\
+	-o "${1%%.bam*}_${outname}"\
+	$sample 2>&1 | tee -a ../*log
+fi
+	
+if [[ $layout == "PE" ]]; then 
+
+featureCounts\
+	-F GTF\
+	-p\		# paired end
+	-C\		# Do not count read pairs matching different chromosomes
+	-T 4\
+	-f\		# Perform feature level counting
+	-t exon\	# feature = exon
+	-g gene_id\
+	-O\		# Reads assigned to multiple meta-features (exons)
+	-s $strand\
+	-a $bedfile\
+	-o "${1%%.bam*}_${outname}"\
+	$sample 2>&1 | tee -a ../*log
+fi
+
+echo "DONE"
