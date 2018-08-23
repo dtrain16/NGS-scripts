c6503bc306044d03eae6d485b2e1e7e44eb0fdbf
d_train16 <diepganguly@Gmail.com>
2018-08-23 13:48:33 +1000

    minor

--- MethylC/DSS_file_prep.r
+++ MethylC/DSS_file_prep.r
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
-# Produce DSS input files from bismark cov files
+# Produce DSS input files from BED files
 # run in folder with cov files of interest and tell which context you want to merge together
-# USAGE: DSS_file_prep.r <context: CpG, CHG, CHH>
+# USAGE: DSS_file_prep.r <context>
 
 options(echo=T)
 args=commandArgs(trailingOnly=T)
@@ -11,14 +11,13 @@ print(args)
 context = args[1]
 
 ## get cov files 
-files=dir(pattern=paste0(context,".bed.bismark.cov"))
+files=dir(pattern=paste0(context,".bed"))
 
 ## get total and met counts for each sample in given context and write out as separate files
 for(i in 1:length(files)){
 file <- read.delim(files[i], head=F)
 file <- file[file$V1 != "ChrM",]
 file <- file[file$V1 != "ChrC",]
-file[,7] <- file[,5] + file[,6]
 file <- file[,c(1,2,7,5)]
 test <- as.numeric(regexec(text = as.character(files[i]), pattern=".bed"))
 sample <- substr(as.character(files[i]), start = 1, stop = test-1)
