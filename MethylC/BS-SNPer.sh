9c4ced23a02b310ba4a17f64a2d33ef279026c1e
d_train16 <diepganguly@Gmail.com>
2018-09-08 14:40:59 +1000

    minor updates

--- MethylC/BS-SNPer.sh
+++ MethylC/BS-SNPer.sh
@@ -6,15 +6,18 @@
 # perl
 # BS-SNPer http://bioinformatics.oxfordjournals.org/content/31/24/4006.long
 
-if [ "$#" -ne 1 ]; then
-echo "USAGE: <file>_trimmed.fq_bismark.sorted.bam"
-echo "EXAMPLE: BS-SNPer.sh 277_D9"
+if [ "$#" -ne 3 ]; then
+echo "USAGE: <file> <fasta> <outname>"
+echo "EXAMPLE: BS-SNPer.sh alx8-r1.sorted.bam $HOME/TAIR10/TAIR10_Chr.all.fasta alx8-r1"
 exit 1
 fi
 
 file=$1
+fa=$2
+out=$3
 
-perl ~/bin/BS-Snper-master/BS-Snper.pl --fa ~/TAIR10_bs/Arabidopsis_thaliana.TAIR10.22.dna.genome.fa --input ${file}_trimmed.fq_bismark.sorted.bam --output temp.out --methcg meth.cg --methchg meth.chg --methchh meth.chh --minhetfreq 0.15 --minhomfreq 0.85 --minquali 30 --mincover 15 --maxcover 1000 --minread2 2 --errorate 0.02 --mapvalue 20 > ${file}.SNP.bed 2>${file}_ERR.log
+perl ~/bin/BS-Snper-master/BS-Snper.pl --fa $fa --input $file --output temp.out --methcg meth.cg --methchg meth.chg --methchh meth.chh --minhetfreq 0.15 --minhomfreq 0.85 --minquali 30 --mincover 15 --maxcover 1000 --minread2 2 --errorate 0.02 --mapvalue 20 > ${out}.SNP.bed 2>${out}_ERR.log
 
 sort -k1,1 -k2,2n ${file}.SNP.bed > ${file}.SNP.sorted.bed
-sed -i '/SNP finished/d' ${file}.SNP.sorted.bed
+rm ${out}.SNP.bed
+
