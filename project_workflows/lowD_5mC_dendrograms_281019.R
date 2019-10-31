04cfd8a91ec2ae717ea65d55aae5fc950ee7f2c5
d_train16 <diepganguly@Gmail.com>
2019-10-31 12:47:01 +1100

    minor edits

--- project_workflows/lowD_5mC_dendrograms_281019.R
+++ project_workflows/lowD_5mC_dendrograms_281019.R
@@ -2,7 +2,7 @@
 
 # libraries
 library(tidyverse)
-library(dendextend)
+suppressPackageStartupMessages(library(dendextend))
 library(pheatmap)
 
 # read in metadata file
@@ -16,6 +16,7 @@ my_grps <- data.frame(acc = unique(meta$Accession)) %>%
 # read in tiled 5mC
 i <- "CG_alltiles_merged_2017-05-05.txt"
 
+## CG = "CG_alltiles_merged_2017-05-05.txt"
 ## CHG = "CHG_alltiles_merged.2017-05-04.txt"
 ## CHH = "CHH_alltiles_merged.2017-05-09.txt"
 
@@ -34,14 +35,7 @@ a <- spread(a, acc, avg_met)
 
 # produce correlation matrix & heatmap
 cor_matrix=as.matrix(cor(a[,3:ncol(a)],use='pairwise.complete.obs'))
-pheatmap(cor_matrix, cutree_rows = 6, annotation = my_grps, fontsize = 4)
-dev.off()
-
-# hierarchical clustering and dendrogram
-hc <- hclust(as.dist(1-cor(as.matrix(a[,3:ncol(a)]),use='pairwise.complete.obs')))
-hc <- as.dendrogram(hc)
-dendro_colors=as.numeric(meta$ClonalGroup)
-labels_colors(hc) = dendro_colors[order.dendrogram(hc)]
-plot(hc)
+pheatmap(cor_matrix, cutree_cols = 7, cutree_rows = 7, annotation_col=my_grps, 
+		annotation_row= my_grps, fontsize = 4)
 dev.off()
 
