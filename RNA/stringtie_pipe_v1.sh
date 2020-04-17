1b38710f50df0b5d3259ddb6dec933b60c6f9310
Diep Ganguly <diepg@flashheart.anu.edu.au>
2020-04-17 17:48:01 +1000

    stringtie pipe

---
+++ RNA/stringtie_pipe_v1.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+set -eu
+
+# StringTie2 pipeline: assemble transcripts using aligned reads (BAM output e.g. from Subjunc or Hisat2), merge assemblies into combined annotation, and quantify transcript abundance (TPM) across each sample in the dataset. 
+# software required: samtools, stringtie.
+
+## e.g. generate list of sample names 
+## dir *bam > files.txt
+
+if [ "$#" -lt 2 ]; then
+echo "Missing required arguments!"
+echo "USAGE: stringtie_pipe_v1.sh <sample list> <library strandedness> <reference GFF3/GTF>"
+echo "EXAMPLE: stringtie_pipe_v1.sh files.txt <un / fr / rf> Arabidopsis_thaliana.TAIR10.46.gff3"
+exit 1
+fi
+
+## gather input variables
+smpls=$(cat $1)
+type=$2
+ref=$3
+dow=$(date +"%F-%H-%m")
+
+echo ""
+echo "Organising BAM files"
+echo ""
+
+## ensure all BAM files are sorted and indexed
+new_smpls=""
+
+for i in $smpls; 
+	do
+	if [[ $i != *sorted.bam ]]; then 
+		samtools sort -@ 4 $i -o "${i%%.bam}.sorted.bam"
+		fq="${i%%.bam}.sorted.bam"
+		samtools index $fq		
+		new_smpls="$new_smpls $fq";
+	else new_smpls="$smpls";
+	fi;
+done
+
+echo ""
+echo "Transcript assembly per sample"
+echo ""
+
+## Use each sample to produce transcript assembly
+
+if [ "$type" == "un" ] && [ -z "$ref" ]; then
+
+echo ""
+echo "Unstranded library with no reference annotation"
+echo ""
+
+for i in $new_smpls; do
+	stringtie $i -o "${i%%.sorted.bam}_stringtie.out" 2>&1 | tee -a ${dow}_stringtie.log;
+	done
+fi
+
+if [ "$type" == "un" ] && [ ! -z "$ref" ]; then
+
+echo ""
+echo "Unstranded library with annotation $ref"
+echo ""
+
+for i in $new_smpls; do
+        stringtie $i -G $ref -o "${i%%.sorted.bam}_stringtie.out" 2>&1 | tee -a ${dow}_stringtie.log;
+	done
+fi
+
+if [ "$type" == "fr" ] && [ -z "$ref" ]; then
+
+echo ""
+echo "Forward stranded without reference annotation"
+echo ""
+
+for i in $new_smpls; do
+	stringtie $i --fr -o "${i%%.sorted.bam}_stringtie.out" 2>&1 | tee -a ${dow}_stringtie.log;
+	done
+fi
+
+if [ "$type" == "fr" ] && [ ! -z "$ref" ]; then
+
+echo ""
+echo "Forward stranded with reference $ref"
+echo ""
+
+for i in $new_smpls; do
+	stringtie $i --fr -G $ref -o "${i%%.sorted.bam}_stringtie.out" 2>&1 | tee -a ${dow}_stringtie.log;
+	done
+fi
+
+if [ "$type" == "rf" ]  && [ -z "$ref" ]; then
+
+echo ""
+echo "Reverse stranded with reference annotation"
+echo ""
+
+for i in $new_smpls; do
+	stringtie $i --rf -o "${i%%.sorted.bam}_stringtie.out" 2>&1 | tee -a ${dow}_stringtie.log;
+	done
+fi
+
+if [ "$type" == "rf" ]  && [ ! -z "$ref" ]; then
+
+echo ""
+echo "Reverse stranded with reference $ref"
+echo ""
+
+for i in $new_smpls; do
+        stringtie $i --rf -G $ref -o "${i%%.sorted.bam}_stringtie.out" 2>&1 | tee -a ${dow}_stringtie.log;
+        done
+fi
+
+## Merge assemblies
+echo ""
+echo "Merging assemblies"
+echo ""
+
+strng="*_stringtie.out"
+
+if [ ! -z "$ref" ]; then 
+	stringtie --merge -i $strng -G $ref -o "merged_stringtie_out.gtf" 2>&1 | tee -a ${dow}_stringtie.log; 
+	else stringtie --merge -i $strng -o "merged_stringtie.gtf" 2>&1 | tee -a ${dow}_stringtie.log;
+fi
+
+## clean-up
+rm *_stringtie.out
+
+## Determine abundance of assembled transcript in each sample
+
+echo ""
+echo "Abundance estimation"
+echo ""
+
+if [ "$type" == "un" ]; then
+
+for i in $new_smpls; do
+	stringtie $i -eA -G merged_stringtie.gtf -o "${i%%.sorted.bam}_abundance.out 2>&1 | tee -a ${dow}_stringtie.log;
+	done
+fi
+
+if [ "$type" == "fr" ]; then
+
+for i in $new_smpls; do
+        stringtie $i -eA --fr -G merged_stringtie.gtf -o "${i%%.sorted.bam}_abundance.out 2>&1 | tee -a ${dow}_stringtie.log;
+        done
+fi
+
+if [ "$type" == "rf" ]; then
+
+for i in $new_smpls; do
+        stringtie $i -eA --rf -G merged_stringtie.gtf -o "${i%%.sorted.bam}_abundance.out 2>&1 | tee -a ${dow}_stringtie.log;
+        done
+fi
+
+echo "Complete!"
+##############################
+
+
