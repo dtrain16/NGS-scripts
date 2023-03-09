dfde654c932acc5ffdb8afcbb6ab2208cfccaeaf
dganguly <dganguly@gregory-compute04.sas.upenn.edu>
2023-03-09 15:50:50 -0500

    EJC bound rnas

--- RNA/ejc_bound_rna.r
+++ RNA/ejc_bound_rna.r
@@ -2,7 +2,6 @@
 # See BAM_to_EJC.sh to get input files ("10bp.50.bed")
 # Determine transripts with differential occupancy of EJC complex, inferred by abundance of 5'P reads 30-25 nt upstream of the 3' end of an exon.
 
-library(fields)
 library(tidyverse)
 
 fls <- dir(pattern = "_10bp.5p.bed")
@@ -27,8 +26,7 @@ out <- rbind(input,out)
 
 }
 
-test <- mutate(out, genotype = sapply(strsplit(sample, "_"), function(l) l[1])) %>%
-	group_by(sample, genotype, V8) %>%
+test <- group_by(out, sample, V8) %>%
 	## sum read end depth
 	summarise(sum_val = sum(val)) %>%
 	spread(sample, sum_val)
@@ -36,20 +34,18 @@ test <- mutate(out, genotype = sapply(strsplit(sample, "_"), function(l) l[1])) 
 ### NAs = 0 / no coverage
 test[is.na(test)] <- 0
 ## at least 3 read ends in 2 samples
-keep <- rowSums(test[3:6] > 3) > 1
+keep <- rowSums(test[2:5] > 5) > 1
 y <- test[keep,]
 
-y1 <- 	gather(y,sample, val, -genotype, -V8) %>%
+y1 <- 	gather(y, sample, val, -V8) %>%
+	mutate(genotype = sapply(strsplit(sample, "_"), function(l) l[1])) %>%
 	group_by(genotype, V8) %>%
 	# mean read ends per genotype
 	summarise(mean_val = mean(val)) %>% 
-	spread(genotype, mean_val)
-
-test[is.na(test)] <- 0
-test <- mutate(test, fc = (dxo1+1)/(WT+1))
-median(test$fc)
-ejc_out <- subset(test, fc > median(test$fc)) 
+	spread(genotype, mean_val) %>%
+	mutate(fc = (dxo1+1)/(WT+1))
 
 ## output
-write.table(ejc_out, "ejc_rna_dxo1.txt", sep='\t', quote=F, row.names=F)
+write.table(y1, "ejc_rna_dxo1.txt", sep='\t', quote=F, row.names=F)
+
 
