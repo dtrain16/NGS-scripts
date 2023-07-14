2f54a5c73262b72e8ae02b0793c49ad586471539
dganguly <dganguly@gregory-compute04.sas.upenn.edu>
2023-07-14 15:39:10 -0400

    minor

--- RNA/macs_peaks.sh
+++ RNA/macs_peaks.sh
@@ -189,10 +189,10 @@ macs2 callpeak --nomodel --extsize 50 -c ${input%%bam}plus.bam -t ${ip%%bam}plus
 
 macs2 callpeak --nomodel --extsize 50 -c ${input%%bam}minus.bam -t ${ip%%bam}minus.bam -f BAM -g 32542107 -n ${out}_minus -q 1e-2
 
-## generate bedfile with strand information using .narrowpeak
-awk 'BEGIN {OFS="\t";FS="\t"} $ awk {print $1,$2,$3,$4,$5, "-", $7, $9, $10}' ${out}_minus_peaks.narrowPeak > ${out}_minus_peaks.stranded.bed
+## generate bedfile with strand information using .narrowpeak ($7 = enrichment, $10 distance to peak)
+awk 'BEGIN {OFS="\t";FS="\t"} $ awk {print $1,$2,$3,$4,$7, "-",$10}' ${out}_minus_peaks.narrowPeak > ${out}_minus_peaks.stranded.bed
 
-awk 'BEGIN {OFS="\t";FS="\t"} $ awk {print $1,$2,$3,$4,$5, "+", $7, $9, $10}' ${out}_plus_peaks.narrowPeak > ${out}_plus_peaks.stranded.bed
+awk 'BEGIN {OFS="\t";FS="\t"} $ awk {print $1,$2,$3,$4,$7, "+",$10}' ${out}_plus_peaks.narrowPeak > ${out}_plus_peaks.stranded.bed
 
 ## merge and sort by coordinate
 cat ${out}_minus_peaks.stranded.bed ${out}_plus_peaks.stranded.bed | sort -k1,1 -k2,2n > ${out}_merged_peaks.strand.bed &
