3d72600d416d655285de5ecb77bf13705a0d94eb
dganguly <dganguly@gregory-compute04.sas.upenn.edu>
2023-11-17 17:00:41 -0500

    gmuct chaos

--- RNA/BAM_to_STOP.sh
+++ RNA/BAM_to_STOP.sh
@@ -12,11 +12,11 @@ set -u
 # conda install -n ngs_plots -c r r-tidyverse
 # conda activate ngs_plots
 
-if [ "$#" -lt 4 ]; then
+if [ "$#" -lt 5 ]; then
 echo "Missing arguments!"
 echo "USAGE: BAM_to_STOP.sh <.BAM> <strandedness> <bedfile annotation> <feature name>"
 echo "strandedness = unstranded or  forward"
-echo "EXAMPLE: BAM_to_STOP.sh col0_rep1.sorted.bam unstranded Arabidopsis_thaliana.TAIR10.54_stop.bed stop"
+echo "EXAMPLE: BAM_to_STOP.sh col0_rep1.sorted.bam unstranded Arabidopsis_thaliana.TAIR10.54_stop.bed stop 50"
 echo "annotation should be start or stop codons, manually calculated from TAIR10 Ensembl UTR annotations"
 exit 1
 fi
@@ -25,12 +25,14 @@ smp=$1
 str=$2
 bedfile=$3
 out=$4
+dis=$5
 
 echo ""
 echo "sample = $1"
 echo "strand = $2"
 echo "bedfile = $3"
 echo "feature = $4"
+echo "distance = $5"
 echo ""
 
 if [[ "$str"  == "unstranded" ]] ; then 
@@ -38,10 +40,10 @@ if [[ "$str"  == "unstranded" ]] ; then
 	echo "BAM to bed..."
 	bedtools genomecov -bg -5 -ibam $smp > ${smp%%.bam}.5p.bed
 	closestBed -D "b" -a ${smp%%.bam}.5p.bed -b $bedfile > ${smp%%.bam}_${out}.5p.bed
-	awk -F$'\t' '$NF<50 && $NF>-50' ${smp%%.bam}_${out}.5p.bed > ${smp%%.bam}_${out}_50bp.5p.bed 
+	awk -F$'\t' -v a=$dis '$NF<a && $NF>-a' ${smp%%.bam}_${out}.5p.bed > ${smp%%.bam}_${out}_${dis}bp.5p.bed 
 
 	echo 'do maths'
-	Rscript /home/dganguly/scripts/RNA/rel_expression_plots_stop.r ${smp%%.bam}_${out}_50bp.5p.bed
+	Rscript /home/dganguly/scripts/RNA/rel_expression_plots_stop.r ${smp%%.bam}_${out}_${dis}bp.5p.bed
 
 	echo 'cleaning'
 	rm -v ${smp%%.bam}.5p.bed ${smp%%.bam}_${out}.5p.bed
@@ -63,16 +65,16 @@ if [[ "$str"  == "forward" ]] ; then
 	# minus strand
 	bedtools genomecov -bg -5 -scale -1 -ibam ${smp%%bam}reverse.bam > ${smp%%.bam}.minus.5p.bed
 	closestBed -D "b" -a ${smp%%.bam}.minus.5p.bed -b $bedfile > ${smp%%.bam}_${out}.minus.5p.bed
-        awk -F$'\t' '$NF<50 && $NF>-50' ${smp%%.bam}_${out}.minus.5p.bed > ${smp%%.bam}_${out}_50bp.minus.5p.bed
+        awk -F$'\t' -v a=$dis '$NF<a && $NF>-a' ${smp%%.bam}_${out}.minus.5p.bed > ${smp%%.bam}_${out}_${dis}bp.minus.5p.bed
 
 	# plus strand
 	bedtools genomecov -bg -5 -ibam ${smp%%bam}forward.bam > ${smp%%.bam}.plus.5p.bed
 	closestBed -D "b" -a ${smp%%.bam}.plus.5p.bed -b $bedfile > ${smp%%.bam}_${out}.plus.5p.bed
-        awk -F$'\t' '$NF<50 && $NF>-50' ${smp%%.bam}_${out}.plus.5p.bed > ${smp%%.bam}_${out}_50bp.plus.5p.bed
+        awk -F$'\t' -v a=$dis '$NF<a && $NF>-a' ${smp%%.bam}_${out}.plus.5p.bed > ${smp%%.bam}_${out}_${dis}bp.plus.5p.bed
 
 	echo 'do maths'
-	Rscript /home/dganguly/scripts/RNA/rel_expression_plots_stop.r ${smp%%.bam}_${out}_50bp.minus.5p.bed
-	Rscript /home/dganguly/scripts/RNA/rel_expression_plots_stop.r ${smp%%.bam}_${out}_50bp.plus.5p.bed
+	Rscript /home/dganguly/scripts/RNA/rel_expression_plots_stop.r ${smp%%.bam}_${out}_${dis}bp.minus.5p.bed
+	Rscript /home/dganguly/scripts/RNA/rel_expression_plots_stop.r ${smp%%.bam}_${out}_${dis}bp.plus.5p.bed
 	
 	echo "Cleaning"
 	rm -v  ${smp%%bam}reverse.bam ${smp%%bam}forward.bam ${smp%%.bam}.minus.5p.bed ${smp%%.bam}_${out}.minus.5p.bed ${smp%%.bam}.plus.5p.bed ${smp%%.bam}_${out}.plus.5p.bed
@@ -95,16 +97,16 @@ if [[ "$str"  == "reverse" ]] ; then
         # minus strand
         bedtools genomecov -bg -5 -scale -1 -ibam ${smp%%bam}forward.bam > ${smp%%.bam}.minus.5p.bed
         closestBed -D "b" -a ${smp%%.bam}.minus.5p.bed -b $bedfile > ${smp%%.bam}_${out}.minus.5p.bed
-        awk -F$'\t' '$NF<50 && $NF>-50' ${smp%%.bam}_${out}.minus.5p.bed > ${smp%%.bam}_${out}_50bp.minus.5p.bed
+        awk -F$'\t' -v a=$dis '$NF<a && $NF>-a' ${smp%%.bam}_${out}.minus.5p.bed > ${smp%%.bam}_${out}_${dis}bp.minus.5p.bed
 
         # plus strand
         bedtools genomecov -bg -5 -ibam ${smp%%bam}reverse.bam > ${smp%%.bam}.plus.5p.bed
         closestBed -D "b" -a ${smp%%.bam}.plus.5p.bed -b $bedfile > ${smp%%.bam}_${out}.plus.5p.bed
-        awk -F$'\t' '$NF<50 && $NF>-50' ${smp%%.bam}_${out}.plus.5p.bed > ${smp%%.bam}_${out}_50bp.plus.5p.bed
+        awk -F$'\t' -v a=$dis '$NF<a && $NF>-a' ${smp%%.bam}_${out}.plus.5p.bed > ${smp%%.bam}_${out}_${dis}bp.plus.5p.bed
 
         echo 'do maths'
-        Rscript /home/dganguly/scripts/RNA/rel_expression_plots_stop.r ${smp%%.bam}_${out}_50bp.minus.5p.bed
-        Rscript /home/dganguly/scripts/RNA/rel_expression_plots_stop.r ${smp%%.bam}_${out}_50bp.plus.5p.bed
+        Rscript /home/dganguly/scripts/RNA/rel_expression_plots_stop.r ${smp%%.bam}_${out}_${dis}bp.minus.5p.bed
+        Rscript /home/dganguly/scripts/RNA/rel_expression_plots_stop.r ${smp%%.bam}_${out}_${dis}bp.plus.5p.bed
 
         echo "Cleaning"
         rm -v  ${smp%%bam}reverse.bam ${smp%%bam}forward.bam ${smp%%.bam}.minus.5p.bed ${smp%%.bam}_${out}.minus.5p.bed ${smp%%.bam}.plus.5p.bed ${smp%%.bam}_${out}.plus.5p.bed
--- RNA/BAM_to_bedgraph_5p.sh
+++ RNA/BAM_to_bedgraph_5p.sh
@@ -12,73 +12,36 @@ set -eu
 
 if [ "$#" -lt 4 ]; then
 echo "Missing arguments!"
-echo "USAGE: BAM_to_bedgraph_5p.sh <.BAM> <strandedness> <bedfile annotation> <feature name>"
-echo "strandedness = unstranded or forward"
-echo "EXAMPLE: BAM_to_bedgraph_5p.sh col0_rep1.sorted.bam unstranded Arabidopsis_thaliana.TAIR10.54_exon.bed exon"
+echo "USAGE: BAM_to_bedgraph_5p.sh <.BAM> <bedfile annotation> <feature name> <distance>"
+echo "EXAMPLE: BAM_to_bedgraph_5p.sh col0_rep1.sorted.bam Arabidopsis_thaliana.TAIR10.54_stop.bed stop 50"
 exit 1
 fi
 
 smp=$1
-str=$2
-bedfile=$3
-out=$4
+bedfile=$2
+out=$3
+dis=$4
 
 echo ""
 echo "sample = $1"
-echo "strand = $2"
-echo "bedfile = $3"
-echo "feature = $4"
+echo "bedfile = $2"
+echo "feature = $3"
+echo "distance = $4"
 echo ""
 
-if [[ "$str"  == "unstranded" ]] ; then 
+echo "BAM to bed..."
+bedtools genomecov -bg -5 -ibam $smp > ${smp%%bam}5p.bed
 
-	echo "BAM to bed..."
-	bedtools genomecov -bg -5 -ibam $smp > ${smp%%bam}5p.bed
+echo 'bedtools for coverage across chosen features...'
+closestBed -D "b" -a ${smp%%bam}5p.bed -b $bedfile > ${smp%%.bam}_${out}.5p.bed
 
-	echo 'bedtools for coverage across exons...'
-	closestBed -D "b" -a ${smp%%bam}5p.bed -b $bedfile > ${smp%%.bam}_${out}.5p.bed
+echo 'subset ...'
+awk -F$'\t' -v a=$dis '$NF<a && $NF>-a' ${smp%%.bam}_${out}.5p.bed > ${smp%%.bam}_${out}_${dis}bp.5p.bed
 
-	echo 'subset ...'
-	awk -F$'\t' '$NF<100 && $NF>-100' ${smp%%.bam}_${out}.5p.bed > ${smp%%.bam}_${out}_100bp.5p.bed
-
-	echo 'do maths'
-	Rscript /home/dganguly/scripts/RNA/rel_expression_plots.r ${smp%%.bam}_${out}_100bp.5p.bed
+echo 'do maths'
+Rscript /home/dganguly/scripts/RNA/rel_expression_plots.r ${smp%%.bam}_${out}_${dis}bp.5p.bed
 	
-	echo 'cleaning'
-	rm -v ${smp%%bam}5p.bed ${smp%%.bam}_${out}.5p.bed ${smp%%.bam}_${out}_100bp.5p.bed
-
-fi
-
-if [[ "$str"  == "forward" ]] ; then
-
-	# https://www.biostars.org/p/179035/
-	# extract reads from + and - strand
-	
-	echo 'stranded BAMs'
-	# map to reverse strand
-	samtools view -@ 2 -f 16 -b $smp > ${smp%%bam}reverse.bam
-	# map to foward strand
-	samtools view -@ 2 -F 16 -b $smp > ${smp%%bam}forward.bam
-	
-	echo "BAM to bedgraphs at exons ..."
-	# minus strand
-	bedtools genomecov -bg -5 i-scale -1 -ibam ${smp%%bam}reverse.bam > ${smp%%bam}minus.5p.bed
-	closestBed -D "b" -a ${smp%%bam}minus.5p.bed -b $bedfile > ${smp%%.bam}_${out}.minus.5p.bed
-	awk -F$'\t' '$NF<100 && $NF>-100' ${smp%%.bam}_${out}.minus.5p.bed > ${smp%%.bam}_${out}_100bp.minus.5p.bed
-
-	# plus strand
-        bedtools genomecov -bg -5 -ibam ${smp%%bam}forward.bam > ${smp%%bam}plus.5p.bed
-        closestBed -D "b" -a ${smp%%bam}plus.5p.bed -b $bedfile > ${smp%%.bam}_${out}.plus.5p.bed
-        awk -F$'\t' '$NF<100 && $NF>-100' ${smp%%.bam}_${out}.plus.5p.bed > ${smp%%.bam}_${out}_100bp.plus.5p.bed
-
-	echo 'do maths'
-	Rscript /home/dganguly/scripts/RNA/rel_expression_plots.r ${smp%%.bam}_${out}_100bp.minus.5p.bed
-	Rscript /home/dganguly/scripts/RNA/rel_expression_plots.r ${smp%%.bam}_${out}_100bp.plus.5p.bed
-
-	echo "Cleaning"
-	rm -v ${smp%%bam}reverse.bam ${smp%%bam}forward.bam ${smp%%bam}minus.5p.bed ${smp%%.bam}_${out}.minus.5p.bed ${smp%%bam}plus.bed ${smp%%.bam}_${out}.plus.bed ${smp%%.bam}_${out}_100bp*bed
-
-fi
-
+echo 'cleaning'
+rm -v ${smp%%bam}5p.bed ${smp%%.bam}_${out}.5p.bed ${smp%%.bam}_${out}_${dis}bp.5p.bed
 
 
