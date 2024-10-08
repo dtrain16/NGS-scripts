ea2c045c5b2c87f3bf22911c262dbcdf69b54336
dganguly <dganguly@gregory-compute04.sas.upenn.edu>
2024-10-08 15:27:41 -0400

    rpm

--- RNA/BAM_to_bedgraph_5p.sh
+++ RNA/BAM_to_bedgraph_5p.sh
@@ -10,27 +10,33 @@ set -eu
 # conda install -n ngs_plots -c r r-tidyverse
 # conda activate ngs_plots
 
-if [ "$#" -lt 4 ]; then
+if [ "$#" -lt 5 ]; then
 echo "Missing arguments!"
-echo "USAGE: BAM_to_bedgraph_5p.sh <.BAM> <bedfile annotation> <feature name> <distance>"
-echo "EXAMPLE: BAM_to_bedgraph_5p.sh col0_rep1.sorted.bam Arabidopsis_thaliana.TAIR10.54_stop.bed stop 50"
+echo "USAGE: BAM_to_bedgraph_5p.sh <.BAM> <layout: SE/PE> <bedfile annotation> <feature name> <distance>"
+echo "EXAMPLE: BAM_to_bedgraph_5p.sh col0_rep1.sorted.bam PE Arabidopsis_thaliana.TAIR10.54_stop.bed stop 50"
 exit 1
 fi
 
 smp=$1
-bedfile=$2
-out=$3
-dis=$4
+lay=$2
+bedfile=$3
+out=$4
+dis=$5
 
 echo ""
 echo "sample = $1"
-echo "bedfile = $2"
-echo "feature = $3"
-echo "distance = $4"
+echo "layout = $2"
+echo "bedfile = $3"
+echo "feature = $4"
+echo "distance = $5"
 echo ""
 
+echo "calculate scaling factor"
+if [[ "$lay" == "SE" ]] ; then scl=$(bc <<< "scale=6;1000000/$(samtools view -F 260 -c $smp)"); fi
+if [[ "$lay" == "PE" ]] ; then scl=$(bc <<< "scale=6;1000000/$(samtools view -F 260 -c $smp)/2"); fi
+
 echo "BAM to bed..."
-bedtools genomecov -bg -5 -ibam $smp > ${smp%%bam}5p.bed
+bedtools genomecov -bg -5 -scale $scl -ibam $smp > ${smp%%bam}5p.bed
 
 echo 'bedtools for coverage across chosen features...'
 closestBed -D "b" -a ${smp%%bam}5p.bed -b $bedfile > ${smp%%.bam}_${out}.5p.bed
