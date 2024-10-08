419a1147036114a949c13dba27970e7d5b6dc699
dganguly <dganguly@gregory-compute04.sas.upenn.edu>
2024-10-08 15:28:35 -0400

    rpm

--- RNA/BAM_to_TSI.sh
+++ RNA/BAM_to_TSI.sh
@@ -10,20 +10,25 @@ set -u
 # conda install -n ngs_plots -c r r-tidyverse
 # conda activate ngs_plots
 
-if [ "$#" -lt 2 ]; then
+if [ "$#" -lt 3 ]; then
 echo "Missing arguments!"
-echo "USAGE: BAM_to_TSI.sh <.BAM> <bedfile annotation>"
-echo "EXAMPLE: BAM_to_TSI.sh col0_rep1.sorted.bam Arabidopsis_thaliana.TAIR10.54_stop.bed"
+echo "USAGE: BAM_to_TSI.sh <.BAM> <layout:SE/PE> <bedfile annotation>"
+echo "EXAMPLE: BAM_to_TSI.sh col0_rep1.sorted.bam PE Arabidopsis_thaliana.TAIR10.54_stop.bed"
 echo "annotation provided should represent coordinates of stop codons"
 exit 1
 fi
 
 smp=$1
-bedfile=$2
+lay=$2
+bedfile=$3
 
-echo ""
 echo "sample = $1"
-echo "bedfile = $2"
+echo "layout = $2"
+echo "bedfile = $3"
+
+echo "calculate scaling factor"
+if [[ "$lay" == "SE" ]] ; then scl=$(bc <<< "scale=6;1000000/$(samtools view -F 260 -c $smp)"); fi
+if [[ "$lay" == "PE" ]] ; then scl=$(bc <<< "scale=6;1000000/$(samtools view -F 260 -c $smp)/2"); fi
 
 echo "BAM to bed..."
 bedtools genomecov -bg -5 -ibam $smp > ${smp%%.bam}.5p.bed
